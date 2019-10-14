e7a9b58a8f682b8df808c1712d5d08cc87bf979c
Peter Wehrfritz <peter.wehrfritz@mailbox.org>
2019-10-14 23:25:42 +0200

    Add unit tests for the communication object

---
+++ UaClient.UnitTests/TestException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Workstation.UaClient
+{
+    public sealed class TestException : Exception
+    {
+        public TestException()
+        {
+        }
+
+        public TestException(string message) : base(message)
+        {
+        }
+
+        public TestException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
