7bb8cc4d01343122b5af7727de46e2eddcd926c3
Jared Beach <jaredbeachdesign@gmail.com>
2019-10-19 11:59:23 -0500

    attempt to send password if it is null

--- UaClient/ServiceModel/Ua/Channels/UaTcpSessionChannel.cs
+++ UaClient/ServiceModel/Ua/Channels/UaTcpSessionChannel.cs
@@ -589,12 +589,15 @@ namespace Workstation.ServiceModel.Ua.Channels
                 }
 
                 var userNameIdentity = (UserNameIdentity)this.UserIdentity;
-                if (userNameIdentity.UserName == null || userNameIdentity.Password == null)
+
+                // if username and password are empty and there is no anonymous token
+                if (userNameIdentity.UserName == null && userNameIdentity.Password == null
+                    && this.RemoteEndpoint.UserIdentityTokens.FirstOrDefault(t => t.TokenType == UserTokenType.Anonymous) == null)
                 {
                     throw new InvalidOperationException("Authentication is not anonymous, but no username and password were provided.");
                 }
 
-                byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(userNameIdentity.Password);
+                byte[] passwordBytes = userNameIdentity.Password != null ? System.Text.Encoding.UTF8.GetBytes(userNameIdentity.Password) : new byte[0];
                 int plainTextLength = passwordBytes.Length + this.RemoteNonce.Length;
                 IBufferedCipher encryptor;
                 byte[] cipherText;
