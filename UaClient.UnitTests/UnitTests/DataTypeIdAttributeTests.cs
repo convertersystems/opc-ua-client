2f05daa9a2391f3675bf1fe2f85c621ac4539f12
Peter Wehrfritz <peter.wehrfritz@mailbox.org>
2019-11-19 23:04:35 +0100

    Add unit tests for DataTypeIdAttribute, UserNameIdentity, MonitoredItemAttribute, UserNameIdentity, X509Identity and XmlEncodingIdAttribute

---
+++ UaClient.UnitTests/UnitTests/DataTypeIdAttributeTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Workstation.ServiceModel.Ua;
+using Xunit;
+
+namespace Workstation.UaClient.UnitTests
+{
+    public class DataTypeIdAttributeTests
+    {
+        public static IEnumerable<object[]> CreateData => ExpandedNodeIdTests.ParseData;
+
+        [MemberData(nameof(CreateData))]
+        [Theory]
+        public void Create(string s, ExpandedNodeId id)
+        {
+            var att = new DataTypeIdAttribute(s);
+
+            att.NodeId
+                .Should().Be(id);
+        }
+    }
+}
---
+++ UaClient.UnitTests/UnitTests/X509IdentityTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.X509;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Workstation.ServiceModel.Ua;
+using Xunit;
+
+namespace Workstation.UaClient.UnitTests
+{
+    public class X509IdentityTests
+    {
+        [Fact]
+        public void CreateNull()
+        {
+            var id = new X509Identity(null, null);
+
+            id.Certificate
+                .Should().BeNull();
+            id.PrivateKey
+                .Should().BeNull();
+        }
+    }
+}
