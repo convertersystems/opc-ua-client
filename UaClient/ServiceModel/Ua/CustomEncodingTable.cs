864850977a39db0345443ae21be93ebdcea5f0fc
Peter Wehrfritz <peter.wehrfritz@mailbox.org>
2020-07-17 23:00:05 +0200

    Add encoding tables

---
+++ UaClient/ServiceModel/Ua/CustomEncodingTable.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Reflection;
+
+namespace Workstation.ServiceModel.Ua
+{
+    public class CustomEncodingTable : IEnumerable<(ExpandedNodeId, Type)>
+    {
+        protected readonly List<(ExpandedNodeId, Type)> encodingTable;
+
+        public CustomEncodingTable()
+        {
+            this.encodingTable = new List<(ExpandedNodeId, Type)>();
+        }
+        
+        public CustomEncodingTable(IEnumerable<(ExpandedNodeId, Type)> encodingTable)
+        {
+            this.encodingTable = encodingTable.ToList();
+        }
+        
+        public void Add(ExpandedNodeId encodingId, Type type)
+        {
+            this.encodingTable.Add((encodingId, type));
+        }
+        
+        public void Add((ExpandedNodeId, Type) item)
+        {
+            this.encodingTable.Add(item);
+        }
+
+        public virtual IEnumerator<(ExpandedNodeId, Type)> GetEnumerator()
+        {
+            return encodingTable.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+    }
+
+    public class CustomEncodingTable<TAttribute> : CustomEncodingTable
+        where TAttribute : Attribute, IEncodingIdAttribute
+    {
+        public CustomEncodingTable()
+        {
+        }
+        
+        public CustomEncodingTable(IEnumerable<(ExpandedNodeId, Type)> encodingTable)
+            : base(encodingTable)
+        {
+        }
+        
+        public CustomEncodingTable(IEnumerable<Type> types)
+        {
+            var table = from type in types
+                        let info = type.GetTypeInfo()
+                        where info.ImplementedInterfaces.Contains(typeof(IEncodable))
+                        let attr = info.GetCustomAttribute<TAttribute>(false)
+                        where attr != null
+                        select (attr.NodeId, type);
+            this.encodingTable.AddRange(table);
+        }
+
+        public CustomEncodingTable(Assembly assembly)
+        {
+            var table = from type in assembly.ExportedTypes
+                        let info = type.GetTypeInfo()
+                        where info.ImplementedInterfaces.Contains(typeof(IEncodable))
+                        let attr = info.GetCustomAttribute<TAttribute>(false)
+                        where attr != null
+                        select (attr.NodeId, type);
+            this.encodingTable.AddRange(table);
+        }
+        
+        public void Add(Type type)
+        {
+            var info = type.GetTypeInfo();
+
+            if (info.ImplementedInterfaces.Contains(typeof(IEncodable)))
+            {
+                var attr = info.GetCustomAttribute<TAttribute>(false);
+                if (attr != null)
+                {
+                    this.Add(attr.NodeId, type);
+                }
+            }
+        }
+    }
+}
---
+++ UaClient/ServiceModel/Ua/MergedEncodingTable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Workstation.ServiceModel.Ua
+{
+    public class MergedEncodingTable : IEnumerable<(ExpandedNodeId, Type)>
+    {
+        private IEnumerable<(ExpandedNodeId, Type)> table = Enumerable.Empty<(ExpandedNodeId,Type)>();
+
+        public void Add(IEnumerable<(ExpandedNodeId, Type)> list)
+        {
+            this.table = this.table.Concat(list);
+        }
+
+        public IEnumerator<(ExpandedNodeId, Type)> GetEnumerator()
+            => this.table.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.table.GetEnumerator();
+    }
+}
