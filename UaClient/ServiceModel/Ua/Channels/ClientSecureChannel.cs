2478e5704c5dc1a8b9f6530b748414db065e289c
Trivalik <3148279+trivalik@users.noreply.github.com>
2023-12-01 10:50:10 +0100

    forward internal errors instead of timeout status code

--- UaClient/ServiceModel/Ua/Channels/ClientSecureChannel.cs
+++ UaClient/ServiceModel/Ua/Channels/ClientSecureChannel.cs
@@ -148,7 +148,13 @@ namespace Workstation.ServiceModel.Ua.Channels
             // TimestampHeader takes care that the RequestHeader property will not be null
             using (var timeoutCts = new CancellationTokenSource((int)request.RequestHeader!.TimeoutHint))
             using (var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(timeoutCts.Token, _channelCts.Token, token))
-            using (var registration = linkedCts.Token.Register(o => ((ServiceOperation)o!).TrySetException(new ServiceResultException(StatusCodes.BadRequestTimeout)), operation, false))
+            using (var registration = linkedCts.Token.Register(o =>
+                {
+                   var statusCode = StatusCodes.BadRequestTimeout;
+                   if (PeakFirstPendingException() is ServiceResultException result)
+                       statusCode = result.StatusCode;
+                   ((ServiceOperation)o!).TrySetException(new ServiceResultException(statusCode));
+                }, operation, false))
             {
                 if (_pendingRequests.Post(operation))
                 {
